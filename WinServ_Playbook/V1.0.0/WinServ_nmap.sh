3e7a296498183496ac683a4c2980dcf2062a2c33
ueueueuehehehehe <119378320+ueueueuehehehehe@users.noreply.github.com>
2024-02-03 00:11:18 +0800

    Added NMAP vulnerability test

---
+++ WinServ_Playbook/V1.0.0/WinServ_nmap.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+start_time=$(date +%s)
+nmap_output="nmap_vulns_$start_time.txt"
+
+sudo nmap -sV -oN "$nmap_output" --script vulners 192.168.1.3
+
+awk '/vulners:/,/^[0-9]+\/[a-zA-Z]+/ {if (!/^[0-9]+\/[a-zA-Z]+/) print $0}' "$nmap_output" > "temp_awk_output.txt"
+
+mv "temp_awk_output.txt" "$nmap_output"
+
+curl -F "chat_id=6680816225" \
+     -F "document=$nmap_output" \
+     -F "disable_notification=true" \
+     -F "caption=Nmap Vulnerability Scan Results" \
+     "https://api.telegram.org/bot6964667296:AAF8Bv8XibWEPeQ4CQCL5htOfEVJTrffhHs/sendDocument"
