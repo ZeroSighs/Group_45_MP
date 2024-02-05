97af3bacd998f7b68331f7dabce8deb72616a746
ueueueuehehehehe <119378320+ueueueuehehehehe@users.noreply.github.com>
2024-02-05 16:55:31 +0800

    Modified NMAP Scan Script

--- WinServ_Playbook/V1.0.0/WinServ_nmap.sh
+++ WinServ_Playbook/V1.0.0/WinServ_nmap.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd /usr/share/scripts/output
+
 start_time=$(date +%s)
 nmap_output="nmap_vulns_$start_time.txt"
 
@@ -7,10 +9,10 @@ sudo nmap -sV -oN "$nmap_output" --script vulners 192.168.1.3
 
 awk '/vulners:/,/^[0-9]+\/[a-zA-Z]+/ {if (!/^[0-9]+\/[a-zA-Z]+/) print $0}' "$nmap_output" > "temp_awk_output.txt"
 
-mv "temp_awk_output.txt" "$nmap_output"
+mv -f "temp_awk_output.txt" "$nmap_output"
 
 curl -F "chat_id=6680816225" \
-     -F "document=$nmap_output" \
+     -F "document=@$nmap_output" \
      -F "disable_notification=true" \
      -F "caption=Nmap Vulnerability Scan Results" \
      "https://api.telegram.org/bot6964667296:AAF8Bv8XibWEPeQ4CQCL5htOfEVJTrffhHs/sendDocument"
